ada58479747aae28761f26d680339765545178be
FillART <129558287+Deadtem333@users.noreply.github.com>
2026-07-02 17:26:46 +0300

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import streamlit as st
 import requests
 from PIL import Image, ImageDraw
-from backend_server import run_in_background
+from back import run_in_background
 
 API_URL = "http://localhost:8000/predict"
 MODEL_OPTIONS = {
